5b4a79cdf94e8b1aee18bcd47dda51fbced6cfef
Kewpie Doll <biodieselchris@gmail.com>
2012-03-25 13:41:29 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GentleWind3212::Application.config.secret_token = '8232f046d5d9a2bcce2b6ecb1a8bf5c3317c3ec57a98771a90558809ab95d67be3c3985ea827c8cca08e72d2d37cd480e22dc7077f863917c95e0d0f74b2f4f3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GentleWind3212::Application.config.session_store :cookie_store, :key => '_gentle-wind-3212_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# GentleWind3212::Application.config.session_store :active_record_store
